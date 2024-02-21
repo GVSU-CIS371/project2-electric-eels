f24746acc086466c810ac81c5ef62d335e6f3798
zeratini <gritterb@mail.gvsu.edu>
2024-02-21 19:47:43 +0000

    Finished project. May need to adjust .innerHTML to alternative functions depending on proj requirements

--- src/data-controller.ts
+++ src/data-controller.ts
@@ -1,5 +1,7 @@
 import { Product, products } from "./data";
 
+
+//Generate HTML for a single product
 function generateProductHTML(product: Product): string {
     return `<div class="store-item">
               <img src="${product.image}" alt="${product.name}" />
@@ -9,16 +11,49 @@ function generateProductHTML(product: Product): string {
             </div>`;
 }
 
+//Ensure that the products are rendered in <main id ="main-container"> within index.html
 function renderProducts(prods: Product[]): void {
-    // your code
+    for(let i = 0; i < prods.length; i++){
+        let productHTML = generateProductHTML(prods[i]);
+        let mainContainer = document.getElementById("main-container");
+        if(mainContainer !== null){
+            mainContainer.innerHTML += productHTML;
+        }
+    }
 }
 
+//Fetch all products of a given category and render the selected products into HTML
 function getByCategory(category: string): void {
-    // your code
+    let filterProducts = products.filter((prod) => prod.category === category);
+    let mainContainer = document.getElementById("main-container");
+    //if(mainContainer !== null){
+        mainContainer.innerHTML = "";
+    //}
+
+    for(let i = 0; i < filterProducts.length; i++){
+        let productHTML = generateProductHTML(filterProducts[i]);
+
+        if(mainContainer !== null){
+            mainContainer.innerHTML += productHTML;
+        }
+    }
 }
 
+//Fetch all products with a rating greater than minRating and render the selected products into HTML
 function getByRating(minRating: number): void {
-    // your code
+    let filterProducts = products.filter((prod) => prod.rating > minRating);
+    let mainContainer = document.getElementById("main-container");
+    //if(mainContainer !== null){
+        mainContainer.innerHTML = "";
+    //}
+
+    for(let i = 0; i < filterProducts.length; i++){
+        let productHTML = generateProductHTML(filterProducts[i]);
+
+        if(mainContainer !== null){
+            mainContainer.innerHTML += productHTML;
+        }
+    }
 }
 
 export { renderProducts, getByCategory, getByRating };
